dac237c3c95c3df90aa4c071793cd663bd6edd76
酒井友太郎 <>
2021-08-17 10:45:13 +0900

    each_with_index

--- index.rb
+++ index.rb
@@ -1,1 +1,12 @@
-puts 'Hello World!!'
+ar = %w'aa bb cc dd'
+
+ar.each_with_index do |v ,index|
+  puts "インデックスの#{index}番目は #{v} です"
+end
+
+# インデックスの0番目は aa です
+# インデックスの1番目は bb です
+# インデックスの2番目は cc です
+# インデックスの3番目は dd です
+
+
